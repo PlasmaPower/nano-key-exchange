3b4dba71a1f42fdebee89ad4f9fc34be2ca039ac
Lee Bousfield <ljbousfield@gmail.com>
2020-05-22 02:15:12 -0500

    Add include stdint.h

--- interface.h
+++ interface.h
@@ -1,6 +1,8 @@
 #ifndef NANO_KEY_EXCHANGE_H
 #define NANO_KEY_EXCHANGE_H
 
+#include "stdint.h"
+
 uint8_t nano_get_shared_key(
     const uint8_t * secret_key,
     const uint8_t * other_public_key,
